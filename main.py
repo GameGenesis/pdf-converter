0f546a607b7a347397712bd3510ee9fe16663e94
GameGenesis <gamegenesisdev@gmail.com>
2023-09-16 14:12:46 -0400

    Now supports as many images as possible
    * Calculates aspect ratio based on max width and height
    
    * Resizes image to maintain aspect ratio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-import random
 from fpdf import FPDF
+from PIL import Image
 
 images = [str(i) for i in os.listdir('images')]
 
@@ -10,20 +10,30 @@ images = [str(i) for i in os.listdir('images')]
 
 pdf = FPDF()
 
-pics_per_page = 2
+pics_per_page = 4
 current_pics = pics_per_page
 
-x = 0
-y = 0
+margin_btw_pics = 10
 
 for image in images:
+    image_path = os.path.join(os.getcwd(), 'images', image)
+
+    width, height = Image.open(image_path).size
+
     current_pics += 1
     if current_pics >= pics_per_page:
         pdf.add_page()
         current_pics = 0
     else:
-        pdf.ln(10)
+        pdf.ln(margin_btw_pics)
 
-    pdf.image(os.path.join(os.getcwd(), 'images', image), w=(pdf.w - pdf.l_margin*2))
+    max_width = pdf.w - pdf.l_margin*2
+    max_height = (pdf.h - pdf.t_margin*2 - margin_btw_pics*pics_per_page) // pics_per_page
+
+    aspect_ratio = min(max_width/width, max_height/height)
+
+    # Keeps width and height (max height)
+    # To fill width, take max width instead (dropdown)
+    pdf.image(image_path, w=width*aspect_ratio, h=height*aspect_ratio)
 
 pdf.output('test.pdf', 'F')
