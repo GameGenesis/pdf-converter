580ef237dfa051ce891251d87a0fb28ef880a2bf
Vic <91158876+somevy@users.noreply.github.com>
2023-09-16 16:40:47 -0400

    Update squaresboxnolabel.py

--- squaresboxnolabel.py
+++ squaresboxnolabel.py
@@ -14,7 +14,20 @@ app.geometry("800x500")
 def openfile():
     return filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
 
+<<<<<<< Updated upstream
 buttonPNG = customtkinter.CTkButton(app, width=120, height=50, text="Add PNG files", command=openfile)
 buttonPNG.pack(padx=20, pady=80)
+=======
+buttonPNG = customtkinter.CTkButton(app, width=120, height=50, text="Add PNG/JPG files", command=openfile)
+buttonPNG.pack(padx=20, pady=20)
+
+nameVar = StringVar()
+
+pathLabel = customtkinter.CTkLabel(app, text="Drag and drop file in the entry box")
+pathLabel.pack(side=TOP)
+
+buttonPNG.drop_target_register(DND_ALL)
+buttonPNG.dnd_bind("<<Drop>>", get_path)
+>>>>>>> Stashed changes
 
 app.mainloop()
