27549ff13c29eb39797a09d322136586b1207c35
GameGenesis <gamegenesisdev@gmail.com>
2023-09-16 15:36:09 -0400

    Update file types accepted

--- squaresboxnolabel.py
+++ squaresboxnolabel.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog
 import customtkinter
 
-
 def button_callback():
     print("open file explorer lul")
 
@@ -12,7 +11,8 @@ app.title("Live Laugh Love PDF Converter")
 app.geometry("800x500")
 
 def openfile():
-    return filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
+    return filedialog.askopenfilename(initialdir = "/",title = "Select file",
+                        filetypes = (("PNG Files","*.png"),("JPG Files","*.jpg"),("JPEG Files","*.jpeg"),("All Files","*.*")))
 
 buttonPNG = customtkinter.CTkButton(app, width=120, height=50, text="Add PNG files", command=openfile)
 buttonPNG.pack(padx=20, pady=80)
