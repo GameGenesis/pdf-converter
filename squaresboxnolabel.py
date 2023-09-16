837f4deff13c8186ed38598d9bded9fe37e8c3a6
Vic <91158876+somevy@users.noreply.github.com>
2023-09-16 15:27:29 -0400

    rename

--- squaresboxnolabel.py
+++ squaresboxnolabel.py
@@ -12,7 +12,7 @@ app.title("Live Laugh Love PDF Converter")
 app.geometry("800x500")
 
 def openfile():
-    return filedialog.askopenfilename()
+    return filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
 
 buttonPNG = customtkinter.CTkButton(app, width=120, height=50, text="Add PNG files", command=openfile)
 buttonPNG.pack(padx=20, pady=80)
