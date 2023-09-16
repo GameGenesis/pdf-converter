064f49a6af7f9820e24857398c5e2f5662af6898
Vic <91158876+somevy@users.noreply.github.com>
2023-09-16 16:47:05 -0400

    Update squaresboxnolabel.py

--- squaresboxnolabel.py
+++ squaresboxnolabel.py
@@ -1,24 +1,28 @@
-from tkinter import*
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import StringVar, TOP, filedialog, ttk
+from tkinterdnd2 import TkinterDnD, DND_ALL
 import customtkinter
 
+class Tk(customtkinter.CTk, TkinterDnD.DnDWrapper):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.TkdndVersion = TkinterDnD._require(self)
 
 def button_callback():
     print("open file explorer lul")
 
-app = customtkinter.CTk()
+app = Tk()
 app.title("Live Laugh Love PDF Converter")
 app.geometry("800x500")
 
 def openfile():
-    return filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
+    filePath = filedialog.askopenfilename(initialdir = "/",title = "Select file",
+                        filetypes = (("PNG Files","*.png"),("JPG Files","*.jpg"),("JPEG Files","*.jpeg"),("All Files","*.*")))
+    pathLabel.configure(text=filePath)
 
-<<<<<<< Updated upstream
+def get_path(event):
+    pathLabel.configure(text=event.data)
+
 buttonPNG = customtkinter.CTkButton(app, width=120, height=50, text="Add PNG files", command=openfile)
-buttonPNG.pack(padx=20, pady=80)
-=======
-buttonPNG = customtkinter.CTkButton(app, width=120, height=50, text="Add PNG/JPG files", command=openfile)
 buttonPNG.pack(padx=20, pady=20)
 
 nameVar = StringVar()
@@ -28,6 +32,5 @@ pathLabel.pack(side=TOP)
 
 buttonPNG.drop_target_register(DND_ALL)
 buttonPNG.dnd_bind("<<Drop>>", get_path)
->>>>>>> Stashed changes
 
 app.mainloop()
